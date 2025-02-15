394739e3b8d1b3e41c5dfd3697f38706f1e41d60
amisadhakal <amisadhakal0@gmail.com>
2025-02-15 12:01:46 +0545

    Create basic.php
    elements

---
+++ basichtml/basic.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML Formatting Test</title>
+    <style>
+        .gray-text {
+            color: gray;
+        }
+    </style>
+</head>
+<body>
+    <b>Bold</b><br>
+    <i>Italic</i><br>
+    <code>Teletype (Monospaced)</code><br>
+    <u>Underlined</u><br>
+    f<sub>0</sub> + f<sub>1</sub><br>
+    x<sup>2</sup> + y<sup>2</sup><br>
+    <small>Smaller</small><br>
+    <big>Bigger</big><br>
+    <s>Strike Through</s><br>
+    <b><i>Bold Italic</i></b><br>
+    <code style="font-size: larger;">Big Monospaced</code><br>
+    <small><i>Small Italic</i></small><br>
+    <span class="gray-text">Gray</span><br>
+    <del>Delete</del><br>
+    <ins>Insert</ins><br>
+</body>
+</html>
